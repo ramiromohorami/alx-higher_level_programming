7da96c42016aa09c46abd26fc8e223125276f262
root <ramiromohorami@gmail.com>
2023-11-30 17:21:21 -0800

    100th

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    from calculator_1 import add, mul, sub, div
+    if len(sys.argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+    a = int(sys.argv[1]
+    b = int(sys.argv[3]
+    operation = sys.argv[2]
+    if operation == "+":
+        print("{} + {} = {}".format(a, b, add(a, b)))
+    elif operation == "-":
+        print("{} - {} = {}".format(a, b, sub(a, b)))
+    elif operation == "*":
+        print("{} * {} = {}".format(a, b, mul(a, b)))
+    elif operation == "/":
+        print("{} / {} = {}".format(a, b, div(a, b)))
+    else:
+        print("Unkown operator. Available operators: +, -, *, and /")
+        exit(1)
