6a79734ebb25c5d0695f1b79e7ec3a2777286cc0
root <ramiromohorami@gmail.com>
2023-12-04 14:12:07 -0800

    seventh task

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    if not matrix:
+        return None
+    for inside in matrix:
+        if len(inside) == 0:
+            print()
+        for elem in range(len(inside)):
+            print("{:d}".format(inside[elem]),
+                    end="\n" if elem is len(inside) - 1 else " ")
