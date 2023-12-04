67fce3b6c73946fe0b1fdf7ba65b7e41c8149eb3
root <ramiromohorami@gmail.com>
2023-12-04 14:29:36 -0800

    eiteth task

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def add_tupple(tuple_a=(), tuple_b=()):
+def add_tuple(tuple_a=(), tuple_b=()):
     a_first = tuple_a[0] if len(tuple_a) > 0 else 0
     a_second = tuple_a[1] if len(tuple_a) > 1 else 0
     b_first = tuple_b[0] if len(tuple_b) > 0 else 0
