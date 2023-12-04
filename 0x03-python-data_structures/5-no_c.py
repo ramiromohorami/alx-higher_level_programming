f4c989ef03588363c9e830cb444067b7a3b98e9e
root <ramiromohorami@gmail.com>
2023-12-04 14:05:31 -0800

    sixth task

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,6 +2,6 @@
 def no_c(my_string):
     result = ""
     for i in range(len(my_string)):
-        if (my_string[i] != 'c' and my_string[i] != 'C':
+        if (my_string[i] != 'c' and my_string[i] != 'C'):
             result += my_string[i]
     return result
