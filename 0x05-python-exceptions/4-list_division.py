a108043b585eaff1d1fc9b9bb9e6ee41b08303ba
root <ramiromohorami@gmail.com>
2023-12-18 13:58:53 -0800

    task 4

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -7,7 +7,7 @@ def list_division(my_list_1, my_list_2, list_length):
             new_list.append(my_list_1[i] / my_list_2[i])
         except ZeroDivisionError:
             new_list.apped(0)
-            print("divison by 0")
+            print("division by 0")
             continue
         except IndexError:
             new_list.append(0)
