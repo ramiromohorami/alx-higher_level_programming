3317d63200b8e97c6fea83e5eebb7765089ec984
root <ramiromohorami@gmail.com>
2023-12-19 15:40:49 -0800

    task 05

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -54,5 +54,5 @@ class Square:
         if self.__size == 0:
             print()
             return
-        for inc in range(self.__size):
+        for i in range(self.__size):
             print("".join(["#" for j in range(self.__size)]))
