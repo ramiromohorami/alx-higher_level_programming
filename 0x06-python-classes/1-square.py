71d989521f9ba3e53354efae9c82a66452733fc2
root <ramiromohorami@gmail.com>
2023-12-19 14:33:42 -0800

    task 01

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Square module."""
+
+class Square:
+    """Defining a square."""
+    def __init__(self, size):
+        """Constructor.
+        Args:
+            size: length
+        """
+        self.__size = size
